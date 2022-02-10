90f54cf86a180ff2608327e592e860015b5cf621
Emily Rodriguez <ecrodriguez@mm279976-pc.lan>
2022-02-10 13:59:27 -0600

    Add javascript action components

---
+++ index.js
@@ -0,0 +1,10 @@
+const core = require('@actions/core');
+const saf = require('@mitre/saf');
+
+try {
+  // `command_string` input defined in action metadata file
+  const command_string = core.getInput('command_string');
+  saf.help()
+} catch (error) {
+  core.setFailed(error.message);
+}
