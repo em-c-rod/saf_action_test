de14af6aed2fb10fb8e9fa87ce06de39389279b6
Emily Rodriguez <ecrodriguez@mm279976-pc.lan>
2022-02-11 11:42:37 -0600

    change index

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const core = require('@actions/core');
 const saf = require('@mitre/saf');
 
-try {
-  // `command_string` input defined in action metadata file
-  const command_string = core.getInput('command_string');
-  saf.run(command_string);
-} catch (error) {
-  core.setFailed(error.message);
-}
+const { exec } = require("child_process");
+exec(command_string, (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(stderr);
+        return;
+    }
+    console.log(stdout);
+});
+
+// try {
+//   // `command_string` input defined in action metadata file
+//   const command_string = core.getInput('command_string');
+//   saf.run(command_string);
+// } catch (error) {
+//   core.setFailed(error.message);
+// }
