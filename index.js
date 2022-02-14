9e3ff754c51b5803e93d38585a34a1aae667c159
Emily Rodriguez <ecrodriguez@mm279976-pc.lan>
2022-02-14 14:36:06 -0600

    cleanup

--- index.js
+++ index.js
@@ -2,20 +2,4 @@ const core = require('@actions/core');
 const saf = require('@mitre/saf');
 
 const command_string = core.getInput('command_string');
-
-const { exec } = require("child_process");
-console.log(__dirname);
-console.log(process.cwd());
-// "PATH=$(npm bin):$PATH "
 saf.run(command_string.split(" "));
-// exec("node_modules/bin " + "saf " + command_string, (error, stdout, stderr) => {
-//     if (error) {
-//         console.log(`error: ${error.message}`);
-//         return;
-//     }
-//     if (stderr) {
-//         console.log(stderr);
-//         return;
-//     }
-//     console.log(stdout);
-// });
