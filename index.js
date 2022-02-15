316975b872839dd0d65c523df2a95754489df0b9
Emily Rodriguez <ecrodriguez@mm279976-pc.lan>
2022-02-15 13:08:22 -0600

    error

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const saf = require('@mitre/saf');
 try {
     const command_string = core.getInput('command_string');
     if(command_string.split(" ")[0] == "view:heimdall") {
-        error("The SAF Action does not support the 'view:heimdall' command. Please reference the documentation for other uses.")
+        throw new Error("The SAF Action does not support the 'view:heimdall' command. Please reference the documentation for other uses.");
     }
     saf.run(command_string.split(" "));
 } catch(error) {
-    error("SAF CLI Command String argument is required.");
+    throw new Error("SAF CLI Command String argument is required.");
 }
