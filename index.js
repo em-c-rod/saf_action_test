1c987ce68fc2b485c29e120822da7c0a44f070a3
Emily Rodriguez <ecrodriguez@mm279976-pc.lan>
2022-02-15 14:25:56 -0600

    change to ES import

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const core = require('@actions/core');
-const saf = require('@mitre/saf');
+import { getInput } from '@actions/core';
+import { run } from '@mitre/saf';
 
-const command_string = core.getInput('command_string');
+const command_string = getInput('command_string');
 if(!command_string) {
     throw new Error("SAF CLI Command String argument is required.");
 }
@@ -10,4 +10,4 @@ if(command_string.split(" ")[0] == "view:heimdall") {
     throw new Error("The SAF Action does not support the 'view:heimdall' command. Please reference the documentation for other uses.")
 }
 
-saf.run(command_string.split(" "));
+run(command_string.split(" "));
