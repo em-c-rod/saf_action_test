4e244202310a7936d9352032313ac68a859a12dd
Emily Rodriguez <ecrodriguez@mm279976-pc.lan>
2022-02-15 10:53:46 -0600

    add catches for improper use

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const core = require('@actions/core');
 const saf = require('@mitre/saf');
 
-const command_string = core.getInput('command_string');
+try {
+    const command_string = core.getInput('command_string');
+} catch(error) {
+    console.log("SAF CLI Command String argument is required.");
+}
+
+if(command_string.split(" ")[0] == "view:heimdall") {
+    console.error("The SAF Action does not support the 'view:heimdall' command. Please reference the documentation for other uses.")
+}
+
 saf.run(command_string.split(" "));
