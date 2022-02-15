ae79096c03230d4adc3e818dd3f1c23d09d49577
Emily Rodriguez <ecrodriguez@mm279976-pc.lan>
2022-02-15 14:07:59 -0600

    check this error

--- index.js
+++ index.js
@@ -6,11 +6,8 @@ if(!command_string) {
     throw new Error("SAF CLI Command String argument is required.");
 }
 
-try {
-    if(command_string.split(" ")[0] == "view:heimdall") {
-        throw new Error("The SAF Action does not support the 'view:heimdall' command. Please reference the documentation for other uses.")
-    }
-    saf.run(command_string.split(" "));
-} catch(error) {
-    console.error(error);
+if(command_string.split(" ")[0] == "view:heimdall") {
+    throw new Error("The SAF Action does not support the 'view:heimdall' command. Please reference the documentation for other uses.")
 }
+
+saf.run(command_string.split(" "));
