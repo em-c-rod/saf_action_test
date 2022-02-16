ae272cf1482d74f1a228e37f9be28e893b12754f
Emily Rodriguez <ecrodriguez@mm279976-pc.lan>
2022-02-16 12:16:55 -0600

    get module from url

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import { getInput } from '@actions/core';
-import { run } from '@mitre/saf';
+const core = require('@actions/core');
+const saf = require('https://www.npmjs.com/package/@mitre/saf');
 
-const command_string = getInput('command_string');
+const command_string = core.getInput('command_string');
 if(!command_string) {
     throw new Error("SAF CLI Command String argument is required.");
 }
@@ -10,4 +10,4 @@ if(command_string.split(" ")[0] == "view:heimdall") {
     throw new Error("The SAF Action does not support the 'view:heimdall' command. Please reference the documentation for other uses.")
 }
 
-run(command_string.split(" "));
+saf.run(command_string.split(" "));
