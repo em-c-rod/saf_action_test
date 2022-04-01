baafecb54b8e6fb83f69c8a267c322540ebecafd
Emily Rodriguez <ecrodriguez@mm279976-pc.lan>
2022-04-01 09:25:45 -0500

    where am i?:

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const process = require('process');
+  
+// Printing current directory
+console.log("Current working directory: ",
+          process.cwd());
+console.log("Current p directory: ",
+          process.env.PWD);
 const saf_action =  require('./run_command.js');
 
 saf_action();
