b62cc55d09b3f3d6f71fc7f3a8988d15df55541c
Emily Rodriguez <32680215+em-c-rod@users.noreply.github.com>
2023-04-17 09:51:18 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const process = require('process');
 const saf_action =  require('./run_command.js');
+import path from 'path'
   
 // Printing current directory
 console.log("Current working directory: ",
@@ -14,4 +15,4 @@ console.log("Current GITHUB_EVENT_PATH: ",
           process.env.GITHUB_EVENT_PATH);
 
 // saf_action({safCLIPath: "/home/runner/work/_actions/mitre/saf_action/main/node_modules/@mitre/saf/lib/index.js"});
-saf_action({safCLIPath: "/home/runner/work/_actions/em-c-rod/saf_action_test/master/node_modules/@mitre/saf/lib/index.js"});
+saf_action({safCLIPath: path.join(process.env.HOME, 'work/_actions/em-c-rod/saf_action_test/master/node_modules/@mitre/saf/lib/index.js')});
