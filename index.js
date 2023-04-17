bb96b43530898e3a822950468dfe9f9519db2639
Emily Rodriguez <32680215+em-c-rod@users.noreply.github.com>
2023-04-17 09:46:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ console.log("Current working directory: ",
           process.cwd());
 console.log("Current p directory: ",
           process.env.PWD);
+console.log("Current HOME: ",
+          process.env.HOME);
+console.log("Current GITHUB_WORKSPACE: ",
+          process.env.GITHUB_WORKSPACE);
+console.log("Current GITHUB_EVENT_PATH: ",
+          process.env.GITHUB_EVENT_PATH);
 
 // saf_action({safCLIPath: "/home/runner/work/_actions/mitre/saf_action/main/node_modules/@mitre/saf/lib/index.js"});
 saf_action({safCLIPath: "/home/runner/work/_actions/em-c-rod/saf_action_test/master/node_modules/@mitre/saf/lib/index.js"});
