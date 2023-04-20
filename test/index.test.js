f5855f3d185837076b03718a513e2052c8c17a27
Emily Rodriguez <ecrodriguez@mm279976-pc.lan>
2023-04-20 10:44:02 -0500

    update workflows and tests
    Signed-off-by: Emily Rodriguez <ecrodriguez@mm279976-pc.lan>

--- test/index.test.js
+++ test/index.test.js
@@ -1,18 +1,17 @@
 const path = require('path');
-const saf_action =  require('../index.js');
+const saf_action =  require('../run_command.js');
 
 describe('SAF Action tests', () => {
   it('should run SAF with a space topic separator in the command string', async () => {
     let inputFilePath = path.resolve('./test/sample_data/red_hat_good.json');
     process.env['INPUT_COMMAND_STRING'] = `view summary -i ${inputFilePath}`;
-    require('../index.js');
-    await saf_action();
+    await saf_action({safCLIPath: "./node_modules/@mitre/saf/lib/index.js"});
     delete process.env['INPUT_COMMAND_STRING'];
   });
-  it('should run SAF with a semicolon topic separator in the command string', async () => {
+  it('should run SAF with a colon topic separator in the command string', async () => {
     let inputFilePath = path.resolve('./test/sample_data/red_hat_good.json');
     process.env['INPUT_COMMAND_STRING'] = `view:summary -i ${inputFilePath}`;
-    await saf_action();
+    await saf_action({safCLIPath: "./node_modules/@mitre/saf/lib/index.js"});
     delete process.env['INPUT_COMMAND_STRING'];
   });
 });
